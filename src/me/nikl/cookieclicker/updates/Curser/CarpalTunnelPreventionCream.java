005232a30126ce7574275e5007e04c60b54cf9d8
Niklas Eicker <nikle1@gmx.de>
2017-07-09 20:03:39 +0200

    add updates

---
+++ src/me/nikl/cookieclicker/updates/Curser/CarpalTunnelPreventionCream.java
@@ -0,0 +1,50 @@
+package me.nikl.cookieclicker.updates.Curser;
+
+import me.nikl.cookieclicker.Game;
+import me.nikl.cookieclicker.Utility;
+import me.nikl.cookieclicker.productions.Productions;
+import me.nikl.cookieclicker.updates.Upgrade;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Niklas on 09.07.2017.
+ *
+ */
+public class CarpalTunnelPreventionCream extends Upgrade{
+
+    public CarpalTunnelPreventionCream(Game game) {
+        super(game);
+        this.cost = 500;
+        productionsRequirements.put(Productions.CURSER, 1);
+
+        icon = new MaterialData(Material.ARROW).toItemStack();
+        icon.setAmount(1);
+
+        ItemMeta meta = icon.getItemMeta();
+        meta.setDisplayName("Carpal tunnel prevention cream");
+
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add("Price: " + Utility.convertHugeNumber(cost));
+        lore.add(ChatColor.AQUA + "The mouse and cursors are twice as efficient.");
+        lore.add("");
+        lore.add(ChatColor.ITALIC + "\"it... it hurts to click...\"");
+
+        meta.setLore(lore);
+        icon.setItemMeta(meta);
+    }
+
+    @Override
+    public void onActivation() {
+        game.cookiesPerClick = game.cookiesPerClick * 2;
+        game.getProduction(Productions.CURSER).multiply(2);
+        active = true;
+    }
+
+
+}
---
+++ src/me/nikl/cookieclicker/updates/Curser/ReinforcedIndexFinger.java
@@ -0,0 +1,50 @@
+package me.nikl.cookieclicker.updates.Curser;
+
+import me.nikl.cookieclicker.Game;
+import me.nikl.cookieclicker.Utility;
+import me.nikl.cookieclicker.productions.Productions;
+import me.nikl.cookieclicker.updates.Upgrade;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Niklas on 09.07.2017.
+ *
+ */
+public class ReinforcedIndexFinger extends Upgrade{
+
+    public ReinforcedIndexFinger(Game game) {
+        super(game);
+        this.cost = 100;
+        productionsRequirements.put(Productions.CURSER, 1);
+
+        icon = new MaterialData(Material.ARROW).toItemStack();
+        icon.setAmount(1);
+
+        ItemMeta meta = icon.getItemMeta();
+        meta.setDisplayName("Reinforced index finger");
+
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add("Price: " + Utility.convertHugeNumber(cost));
+        lore.add(ChatColor.AQUA + "The mouse and cursors are twice as efficient.");
+        lore.add("");
+        lore.add(ChatColor.ITALIC + "\"prod prod\"");
+
+        meta.setLore(lore);
+        icon.setItemMeta(meta);
+    }
+
+    @Override
+    public void onActivation() {
+        game.cookiesPerClick = game.cookiesPerClick * 2;
+        game.getProduction(Productions.CURSER).multiply(2);
+        active = true;
+    }
+
+
+}
