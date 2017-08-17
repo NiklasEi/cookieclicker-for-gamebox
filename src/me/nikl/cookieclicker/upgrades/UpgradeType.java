c783131a0968df972485088014911211cfb5ef36
Niklas Eicker <nikle1@gmx.de>
2017-08-17 20:32:04 +0200

    rename enums for better self-explanation

--- src/me/nikl/cookieclicker/upgrades/UpgradeType.java
+++ src/me/nikl/cookieclicker/upgrades/UpgradeType.java
@@ -4,5 +4,5 @@ package me.nikl.cookieclicker.upgrades;
  * Created by Niklas on 15.08.2017.
  */
 public enum UpgradeType {
-    CLASSIC, CLASSIC_DOUBLE, GAIN_DOUBLE
+    CLASSIC, CLASSIC_MOUSE, GAIN_MOUSE
 }
