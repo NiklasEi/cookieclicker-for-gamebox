79d9172570f50bb36d7661e9ea7217c8165f2a48
Niklas Eicker <nikle1@gmx.de>
2017-09-05 21:37:01 +0200

    add new buildings

--- src/me/nikl/cookieclicker/buildings/Buildings.java
+++ src/me/nikl/cookieclicker/buildings/Buildings.java
@@ -4,5 +4,6 @@ package me.nikl.cookieclicker.buildings;
  * Created by Niklas on 09.07.2017.
  */
 public enum Buildings {
-    CURSOR, FARM, GRANDMA, MINE, FACTORY, BANK
+    CURSOR, FARM, GRANDMA, MINE, FACTORY, BANK, TEMPLE, WIZARD_TOWER, SHIPMENT, ALCHEMY_LAB,
+    PORTAL, TIME_MACHINE, ANTIMATTER_CONDENSER, PRISM
 }
